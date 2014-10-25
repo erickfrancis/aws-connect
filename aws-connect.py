2e36c25766d1e9bd88d16aaa9ffbf62794978e5f
Erick Francis Matias Oliveira <erickfrancis.zip@gmail.com>
2014-10-25 16:56:16 -0200

    Realizando a junção das janelas com o indicador

---
+++ aws-connect.py
@@ -0,0 +1,71 @@
+class AwsConnect:
+    def __init__(self):
+        import gtk
+        import appindicator
+        import os
+
+        self.gtk = gtk
+
+        self.menu = self.gtk.Menu()
+
+        self.indicator = appindicator.Indicator('wallch_indicator',
+                                                '%s/resources/aws.png' % os.path.dirname(os.path.abspath(__file__)),
+                                                appindicator.CATEGORY_APPLICATION_STATUS)
+        self.indicator.set_status(appindicator.STATUS_ACTIVE)
+
+    def get_indicator(self):
+        return self.indicator
+
+    def get_menu(self):
+        return self.menu
+
+    def add_item_menu(self, item):
+        self.get_menu().append(item)
+
+    def add_menu_quit(self):
+        menu_item = self.gtk.MenuItem('Quit')
+        menu_item.connect('activate', self.gtk.main_quit)
+
+        self.add_item_menu(menu_item)
+
+        return menu_item
+
+    def add_menu_manage_accounts(self):
+        menu_item = self.gtk.MenuItem('Manage accounts')
+        menu_item.connect('activate', self.on_menu_item_manage_accounts)
+
+        self.add_item_menu(menu_item)
+
+        return menu_item
+
+    def on_menu_item_manage_accounts(self, widget, event=None):
+        from gui.manage_account import ManageAccount
+
+        manage_account = ManageAccount()
+
+        self.create_window(manage_account.get_container())
+
+    def create_window(self, container):
+        window = self.gtk.Window(self.gtk.WINDOW_TOPLEVEL)
+        window.set_position(self.gtk.WIN_POS_CENTER)
+        window.set_border_width(10)
+
+        window.connect("delete_event", lambda w, e: window.destroy())
+
+        window.add(container)
+
+        window.show_all()
+
+
+aws_connect = AwsConnect()
+
+menu = aws_connect.get_menu()
+
+indicator = aws_connect.get_indicator().set_menu(menu)
+
+aws_connect.add_menu_manage_accounts()
+aws_connect.add_menu_quit()
+
+menu.show_all()
+
+aws_connect.gtk.mainloop()
---
+++ gui/manage_account.py
@@ -0,0 +1,55 @@
+class ManageAccount:
+    def add_account(self, account_name):
+        form = self.accountForm().factoryForm()
+        form.show_all()
+
+        label = self.gtk.Label()
+        label.set_text(account_name)
+
+        self.tabs.append_page(form, label)
+        self.tabs.next_page()
+
+    def on_create_account(self, widget, event=None):
+        message_dialog = self.gtk.MessageDialog(parent=None, flags=0, type=self.gtk.MESSAGE_QUESTION,
+                                               buttons=self.gtk.BUTTONS_OK_CANCEL,
+                                               message_format="Name")
+
+        action_area = message_dialog.get_content_area()
+
+        name_account_entry = self.gtk.Entry()
+        action_area.pack_start(name_account_entry)
+
+        message_dialog.show_all()
+        response = message_dialog.run()
+
+        if response == self.gtk.RESPONSE_OK:
+            self.add_account(name_account_entry.get_text())
+
+        message_dialog.destroy()
+
+    def __init__(self):
+        import pygtk
+
+        pygtk.require('2.0')
+        import gtk
+
+        self.gtk = gtk
+
+        from account_form import AccountForm
+
+        self.accountForm = AccountForm
+
+        self.tabs = self.gtk.Notebook()
+        self.tabs.set_tab_pos(gtk.POS_TOP)
+        self.tabs.set_scrollable(True)
+
+        self.buttonCreate = self.gtk.Button('Create account')
+        self.buttonCreate.connect("clicked", self.on_create_account)
+
+        self.container = self.gtk.VBox()
+
+        self.container.add(self.buttonCreate)
+        self.container.add(self.tabs)
+
+    def get_container(self):
+        return self.container
